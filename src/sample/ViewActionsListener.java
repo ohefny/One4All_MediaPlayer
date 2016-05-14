6db347c04a459c9da7ac64882b800abf42080898
o.hefny@gmail.com <o.hefny@gmail.com>
2016-05-15 01:25:26 +0200

    connecting DesignView To Controller

--- src/sample/ViewActionsListener.java
+++ src/sample/ViewActionsListener.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 
-interface ViewActionsListener {
+public interface ViewActionsListener {
     void onMediaOpen(File file);
 
     void onPause();
